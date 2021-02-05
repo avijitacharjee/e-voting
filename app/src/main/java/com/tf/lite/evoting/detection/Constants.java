9197619b7a15b680303f77b72fbb8fa5c3b4f387
AvijitAcharjee <avijitach@gmail.com>
2021-02-06 02:30:24 +0600

    code clean

--- app/src/main/java/com/tf/lite/evoting/detection/Constants.java
+++ app/src/main/java/com/tf/lite/evoting/detection/Constants.java
@@ -1,9 +1,5 @@
 package com.tf.lite.evoting.detection;
 
-/**
- * Created by Avijit Acharjee on 1/22/2021 at 9:03 PM.
- * Email: avijitach@gmail.com.
- */
 class Constants {
     public static final String SHARED_PREFERENCES_NAME = "app";
 }
